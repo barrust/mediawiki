0c73c791f3692d6c507f475ef1814317b79cbb3c
Tyler Barrus <barrust@gmail.com>
2017-01-02 14:23:51 -0500

    added python3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,12 @@ setuptools.setup(
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite = 'tests',
     test_requires = ['unittest']
